49cd9e9fb27a1ddfd44fe0303a20c90c926295c7
DSnoddy <douglassnoddyjr@gmail.com>
2021-02-01 21:41:21 -0800

    captured user input

---
+++ app.js
@@ -0,0 +1,74 @@
+const printProfileData = profileDataArr => {
+    // This...
+    for(let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    } 
+};
+
+console.log('================');
+
+ // Is the same as this...
+ profileDataArr.forEach((profileItem) => console.log(profileItem));
+
+
+
+
+
+
+
+ 
+// CAPTURE COMMAND-LINE ARGUMENTS
+
+// var profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+
+
+// PROTECT DATA WITH CONST
+
+// const animalArray = ['dog', 'cat', 'pig'];
+
+// animalArray.push('cow');
+
+// const personObj = {
+//     name: 'Lerantino',
+//     age: 99
+// };
+
+// personObj.age = 100;
+// personObj.occupation = 'Developer';
+
+
+
+
+// ES6 ARROW FUNCTIONS
+
+// const printProfileData = profileDataArr => {
+//     console.log(profileDataArr);
+// };
+
+// printProfileData(profileDataArgs);
+
+//  Using function expression syntax
+// const addNums = function(numOne, numTwo) {
+//     return numOne + numTwo;
+// };
+
+// Using new arrow function syntax
+// const  addNums = (numOne, numTwo) => {
+//     return numOne + numTwo;
+// };
+
+    //     *implicit return*
+    // const addNums = (numOne, numTwo) => numOne + numTwo;
+
+    // const sum = addNums(5, 3); // sum would be 8
+
+// PRINT ARGUMENTS ONE AT A TIME
+
+//     // Notice the lack of parentheses around the `profileDataArr` parameter?
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//       console.log(profileDataArr[i]);
+//     }
+//   };
