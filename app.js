ed327759c4e3219905dedebb73bd10b96b8f47b9
DSnoddy <douglassnoddyjr@gmail.com>
2021-02-02 17:53:38 -0800

    generated HTML file

--- app.js
+++ app.js
@@ -1,22 +1,86 @@
-const printProfileData = profileDataArr => {
-    // This...
-    for(let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    } 
-};
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-console.log('================');
+const profileDataArgs = process.argv.slice(2);
 
- // Is the same as this...
- profileDataArr.forEach((profileItem) => console.log(profileItem));
+const [name, github] = profileDataArgs;
 
 
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
 
 
- 
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// // console.log(profileDataArgs);
+// const [name, github] = profileDataArgs;
+
+// const printProfileData = (profileDataArr) => {
+//     console.log(profileDataArr);
+// };
+
+// printProfileData(profileDataArgs);
+
+
+// const generatePage = (userName, githubName) => {
+//     return `
+//   <!DOCTYPE html> 
+//   <html lang="en"> 
+//   <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <title>Portfolio Demo</title>
+//   </head>
+
+//   <body>
+//     <h1>${name}</h1>
+//     <h2><a href="https://github.com/${github}">Github</a></h2>
+//   </body>
+//   </html>
+//   `;
+// };
+
+
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+
+//     if(err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
+
+
+
+
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for(let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     } 
+// };
+
+// console.log('================');
+
+//  // Is the same as this...
+//  profileDataArr.forEach((profileItem) => console.log(profileItem));
+
+
 // CAPTURE COMMAND-LINE ARGUMENTS
 
 // var profileDataArgs = process.argv.slice(2, process.argv.length);
